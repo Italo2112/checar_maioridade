061bc14a1c70e564d7f53524c9b28e93d6d0dd1f
Italo2112 <italoluciano645@gmail.com>
2024-11-19 19:14:33 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+def maioridade():
+    idade = int(input("digite sua idade:"))
+    if idade >= 18:
+        print('voce tem a maior idade penal')
+    else:
+        print('voce tem a moir idade penal')
+
+maioridade()
